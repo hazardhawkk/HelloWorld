c4ff1739e12c9b99ca5145564f29db11513214d5
hazardhawkk <shahzaibzafarshazi@gmail.com>
2022-01-24 10:09:37 +0000

    init commit

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -6,7 +6,12 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your favourite food");
+            string food = Console.ReadLine();
+            Console.WriteLine("Enter your favourite drink");
+            string drink = Console.ReadLine();
+            Console.WriteLine("The cook is making " + food + " for you.");
+            Console.WriteLine("They are also making " + drink + " for youtewttwer");
         }
     }
 }
